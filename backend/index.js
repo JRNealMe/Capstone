dabb135334bad69ad1940d450ca68a09b5b84b99
Jared Neal <jared.neal@me.com>
2022-06-06 17:41:54 -0600

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,54 +1,84 @@
 //required libaries
 const { Sequelize, DataTypes } = require("sequelize");
-const stylist = require("./models/stylist.js");
+const { Stylist, appointment } = require("../models");
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
+const req = require("express/lib/request");
+require('dotenv').config()
 
 
 //helpers
 
-const frontendDir = "../frontend/";
+const frontendDir = "../frontend";
 
 //database connector: sequelize 
 
-const sequelize = new Sequelize('sqlite::memory:');
-
+const sequelize = new Sequelize(process.env.CONNECTION_STRING,{
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+  }  
+});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
   
- const test = async() => {
-    const stacia = stylist( sequelize, DataTypes ).build({ name: "Stacia"})
-    await stacia.save();
-  
-    const stylists = await stylist.findAll();
-console.log(stylists);
-  }
-
-  test();
-  
-
 //webserver: express
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 const port = 3000
 
+const saveAppointment = async (req) => {
+    const appmt = await appointment.create(req.body);
+    res = await appmt.save();
+    console.log (res);
+}
+
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, `${frontendDir}pages/index.html`));
+    res.sendFile(path.join(__dirname, "../frontend/pages/index.html"));
 
 
 });
 
 app.get("/stylist", (req, res) => {
-    res.sendFile(path.join(__dirname, `${frontendDir}pages/stylist.html`));
+    res.sendFile(path.join(__dirname, "../frontend/pages/stylist.html"));
 
 
 });
 
 app.get("/contact", (req, res) => {
-    res.sendFile(path.join(__dirname, `${frontendDir}pages/contact.html`));
+    res.sendFile(path.join(__dirname, "../frontend/pages/contact.html"));
 
 
 });
+
+app.get("/booking", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/pages/booking.html"));
+
+});
+
+app.post("/booking", (req, res) => {
+    console.log(req.body)
+    saveAppointment(req)
+res.send({response: `You appoint has been scheduled for ${req.body.datetime}`})
+
+});
+
 
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`);
---
+++ migrations/20220602021825-create-appointment.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('appointments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      stylist: {
+        type: Sequelize.INTEGER
+      },
+      service: {
+        type: Sequelize.INTEGER
+      },
+      datetime: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('appointments');
+  }
+};
